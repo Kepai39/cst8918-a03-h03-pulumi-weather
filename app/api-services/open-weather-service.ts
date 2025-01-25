cc6aff5a8978ee580782cd281d61d5d52e873bbd
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-01-25 05:48:02 -0500

    Fixed Issue on redis running globally

--- app/api-services/open-weather-service.ts
+++ app/api-services/open-weather-service.ts
@@ -9,7 +9,6 @@ interface FetchWeatherDataParams {
   lon: number
   units: 'standard' | 'metric' | 'imperial'
 }
-
 export async function fetchWeatherData({
   lat,
   lon,
@@ -17,33 +16,11 @@ export async function fetchWeatherData({
 }: FetchWeatherDataParams) {
   const queryString = `lat=${lat}&lon=${lon}&units=${units}`
 
-  console.log(`Checking cache for key: ${queryString}`)
-
   const cacheEntry = await redis.get(queryString)
-  if (cacheEntry) {
-    console.log('Cache hit. Returning cached data.')
-    return JSON.parse(cacheEntry)
-  }
+  if (cacheEntry) return JSON.parse(cacheEntry)
 
-  console.log('Cache miss. Fetching data from API...')
-
-  try {
-    const response = await fetch(`${BASE_URL}?${queryString}&appid=${API_KEY}`)
-    console.log(`API Response Status: ${response.status}`)
-
-    if (!response.ok) {
-      throw new Error(`API request failed with status: ${response.status} - ${response.statusText}`)
-    }
-
-    const data = await response.text()
-    console.log('Fetched weather data:', data) // Log the raw response from the API
-
-    await redis.set(queryString, data, { PX: TEN_MINUTES }) // The PX option sets the expiry time
-    console.log(`Data cached for key: ${queryString} with expiry of ${TEN_MINUTES} ms`)
-
-    return JSON.parse(data)
-  } catch (error) {
-    console.error('Error fetching weather data:', error)
-    throw new Error('Failed to fetch weather data')
-  }
+  const response = await fetch(`${BASE_URL}?${queryString}&appid=${API_KEY}`)
+  const data = await response.text() // avoid an unnecessary extra JSON.stringify
+  await redis.set(queryString, data, {PX: TEN_MINUTES}) // The PX option sets the expiry time
+  return JSON.parse(data)
 }
--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -3,6 +3,7 @@ import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
 import * as docker from '@pulumi/docker'
 import * as containerinstance from '@pulumi/azure-native/containerinstance'
+import * as cache from '@pulumi/azure-native/cache'
 
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
@@ -55,6 +56,32 @@ const registryCredentials = containerregistry
       password: registryCredentials.password,
     },
   })
+  // Create a managed Redis service
+const redis = new cache.Redis(`${prefixName}-redis`, {
+  name: `${prefixName}-weather-cache`,
+  location: 'westus3',
+  resourceGroupName: resourceGroup.name,
+  enableNonSslPort: true,
+  redisVersion: 'Latest',
+  minimumTlsVersion: '1.2',
+  redisConfiguration: {
+    maxmemoryPolicy: 'allkeys-lru'
+  },
+  sku: {
+    name: 'Basic',
+    family: 'C',
+    capacity: 0
+  }
+})
+
+// Extract the auth creds from the deployed Redis service
+const redisAccessKey = cache
+  .listRedisKeysOutput({ name: redis.name, resourceGroupName: resourceGroup.name })
+  .apply(keys => keys.primaryKey)
+
+// Construct the Redis connection string to be passed as an environment variable in the app container
+const redisConnectionString = pulumi.interpolate`rediss://:${redisAccessKey}@${redis.hostName}:${redis.sslPort}`
+
 
   const containerGroup = new containerinstance.ContainerGroup(
     `${prefixName}-container-group`,
@@ -87,6 +114,10 @@ const registryCredentials = containerregistry
             {
               name: 'WEATHER_API_KEY',
               value: config.requireSecret('weatherApiKey')
+            },
+            {
+              name: 'REDIS_URL',
+              value: redisConnectionString
             },
           ],
           resources: {
