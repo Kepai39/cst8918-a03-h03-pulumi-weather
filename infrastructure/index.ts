2d76b7be90c5dfea0e0ef8f3116596bb489a0fbc
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-01-24 22:14:29 -0500

    Added Secret Configuration and Modified Container Environment for Secret

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -88,6 +88,10 @@ const registryCredentials = containerregistry
               name: 'WEATHER_API_KEY',
               value: '16ddb15c9011a98acb42bf2890add65e',
             },
+            {
+              name: 'WEATHER_API_KEY',
+              value: config.requireSecret('weatherApiKey')
+            },
           ],
           resources: {
             requests: {
